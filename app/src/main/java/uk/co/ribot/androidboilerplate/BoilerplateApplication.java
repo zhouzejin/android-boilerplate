a7fcb5717c59c68cbff43a0ea5f3a237ea08310d
Zhou Zejin <zjzhou1@chinaums.com>
2016-09-07 16:31:59 +0800

    update project

--- app/src/main/java/uk/co/ribot/androidboilerplate/BoilerplateApplication.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/BoilerplateApplication.java
@@ -3,9 +3,6 @@ package uk.co.ribot.androidboilerplate;
 import android.app.Application;
 import android.content.Context;
 
-import com.crashlytics.android.Crashlytics;
-
-import io.fabric.sdk.android.Fabric;
 import timber.log.Timber;
 import uk.co.ribot.androidboilerplate.injection.component.ApplicationComponent;
 import uk.co.ribot.androidboilerplate.injection.component.DaggerApplicationComponent;
@@ -21,7 +18,6 @@ public class BoilerplateApplication extends Application  {
 
         if (BuildConfig.DEBUG) {
             Timber.plant(new Timber.DebugTree());
-            Fabric.with(this, new Crashlytics());
         }
     }
 
