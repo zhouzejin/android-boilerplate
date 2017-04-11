d4b71e3dcd26bcff3b736d3244d8780830e5415e
ZhouZejin <zhouzejine@126.com>
2017-04-11 18:30:49 +0800

    Use RxUtil.unsubscribe()

--- app/src/main/java/uk/co/ribot/androidboilerplate/data/SyncService.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/data/SyncService.java
@@ -17,6 +17,7 @@ import uk.co.ribot.androidboilerplate.data.model.bean.Subject;
 import uk.co.ribot.androidboilerplate.utils.AndroidComponentUtil;
 import uk.co.ribot.androidboilerplate.utils.LogUtil;
 import uk.co.ribot.androidboilerplate.utils.NetworkUtil;
+import uk.co.ribot.androidboilerplate.utils.RxUtil;
 
 public class SyncService extends Service {
 
@@ -48,7 +49,7 @@ public class SyncService extends Service {
             return START_NOT_STICKY;
         }
 
-        if (mSubscription != null && !mSubscription.isUnsubscribed()) mSubscription.unsubscribe();
+        RxUtil.unsubscribe(mSubscription);
         mSubscription = mDataManager.syncSubjects()
                 .subscribeOn(Schedulers.io())
                 .subscribe(new Observer<Subject>() {
