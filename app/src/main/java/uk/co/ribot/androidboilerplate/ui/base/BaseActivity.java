42abd79bb083efc11af1f4f0ce0e706405645bc8
Siagian <rhonyabdullah95@gmail.com>
2017-09-14 16:10:36 +0700

    Known bug (CMIIW) (#73)
    * Using long sparse array instead of map for better performance, save outstate before super.
    
    * Follow ribot android guideline

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/base/BaseActivity.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/base/BaseActivity.java
@@ -1,10 +1,9 @@
 package uk.co.ribot.androidboilerplate.ui.base;
 
 import android.os.Bundle;
+import android.support.v4.util.LongSparseArray;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import timber.log.Timber;
@@ -23,7 +22,8 @@ public class BaseActivity extends AppCompatActivity {
 
     private static final String KEY_ACTIVITY_ID = "KEY_ACTIVITY_ID";
     private static final AtomicLong NEXT_ID = new AtomicLong(0);
-    private static final Map<Long, ConfigPersistentComponent> sComponentsMap = new HashMap<>();
+    private static final LongSparseArray<ConfigPersistentComponent>
+            sComponentsMap = new LongSparseArray<>();
 
     private ActivityComponent mActivityComponent;
     private long mActivityId;
@@ -36,16 +36,15 @@ public class BaseActivity extends AppCompatActivity {
         // being called after a configuration change.
         mActivityId = savedInstanceState != null ?
                 savedInstanceState.getLong(KEY_ACTIVITY_ID) : NEXT_ID.getAndIncrement();
-        ConfigPersistentComponent configPersistentComponent;
-        if (!sComponentsMap.containsKey(mActivityId)) {
+
+        ConfigPersistentComponent configPersistentComponent = sComponentsMap.get(mActivityId, null);
+
+        if (configPersistentComponent == null) {
             Timber.i("Creating new ConfigPersistentComponent id=%d", mActivityId);
             configPersistentComponent = DaggerConfigPersistentComponent.builder()
                     .applicationComponent(BoilerplateApplication.get(this).getComponent())
                     .build();
             sComponentsMap.put(mActivityId, configPersistentComponent);
-        } else {
-            Timber.i("Reusing ConfigPersistentComponent id=%d", mActivityId);
-            configPersistentComponent = sComponentsMap.get(mActivityId);
         }
         mActivityComponent = configPersistentComponent.activityComponent(new ActivityModule(this));
     }
