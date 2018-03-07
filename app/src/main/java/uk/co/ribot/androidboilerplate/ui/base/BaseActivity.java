a3438eb6ed982b285b5aea88ab8bbc3d43c54936
ZhouZejin <zhouzejine@126.com>
2018-03-07 18:03:42 +0800

    optimize BaseActivity

--- app/src/main/java/uk/co/ribot/androidboilerplate/ui/base/BaseActivity.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/ui/base/BaseActivity.java
@@ -1,10 +1,9 @@
 package uk.co.ribot.androidboilerplate.ui.base;
 
 import android.os.Bundle;
+import android.support.v4.util.LongSparseArray;
 import android.support.v7.app.AppCompatActivity;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.atomic.AtomicLong;
 
 import uk.co.ribot.androidboilerplate.BoilerplateApplication;
@@ -23,7 +22,8 @@ public class BaseActivity extends AppCompatActivity {
 
     private static final String KEY_ACTIVITY_ID = "KEY_ACTIVITY_ID";
     private static final AtomicLong NEXT_ID = new AtomicLong(0);
-    private static final Map<Long, ConfigPersistentComponent> sComponentsMap = new HashMap<>();
+    private static final LongSparseArray<ConfigPersistentComponent> sComponentsMap =
+            new LongSparseArray<>();
 
     private ConfigPersistentComponent mConfigPersistentComponent;
     private ActivityComponent mActivityComponent;
@@ -37,15 +37,13 @@ public class BaseActivity extends AppCompatActivity {
         // if this is being called after a configuration change.
         mActivityId = savedInstanceState != null ?
                 savedInstanceState.getLong(KEY_ACTIVITY_ID) : NEXT_ID.getAndIncrement();
-        if (!sComponentsMap.containsKey(mActivityId)) {
+        mConfigPersistentComponent = sComponentsMap.get(mActivityId, null);
+        if (mConfigPersistentComponent == null) {
             LogUtil.i("Creating new ConfigPersistentComponent id=%d", mActivityId);
             mConfigPersistentComponent = DaggerConfigPersistentComponent.builder()
                     .applicationComponent(BoilerplateApplication.get(this).getComponent())
                     .build();
             sComponentsMap.put(mActivityId, mConfigPersistentComponent);
-        } else {
-            LogUtil.i("Reusing ConfigPersistentComponent id=%d", mActivityId);
-            mConfigPersistentComponent = sComponentsMap.get(mActivityId);
         }
         mActivityComponent = mConfigPersistentComponent.activityComponent(new ActivityModule(this));
     }
