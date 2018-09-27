030988744e7f5a268dee549abb9b9c258e6e5a3f
Zhou Zejin <zhouzejin@douyu.tv>
2018-09-27 14:44:57 +0800

    convert App to Plugin Host.

--- app/src/main/java/uk/co/ribot/androidboilerplate/BoilerplateApplication.java
+++ app/src/main/java/uk/co/ribot/androidboilerplate/BoilerplateApplication.java
@@ -1,12 +1,49 @@
 package uk.co.ribot.androidboilerplate;
 
+import android.content.Context;
+import android.content.res.Configuration;
+
+import com.qihoo360.replugin.RePlugin;
 import com.sunny.commonbusiness.base.BaseApplication;
 
 public class BoilerplateApplication extends BaseApplication {
 
     @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        RePlugin.App.attachBaseContext(this);
+    }
+
+    @Override
     public void onCreate() {
         super.onCreate();
+
+        RePlugin.App.onCreate();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+
+        /* Not need to be called if your application's minSdkVersion > = 14 */
+        RePlugin.App.onLowMemory();
+    }
+
+    @Override
+    public void onTrimMemory(int level) {
+        super.onTrimMemory(level);
+
+        /* Not need to be called if your application's minSdkVersion > = 14 */
+        RePlugin.App.onTrimMemory(level);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration config) {
+        super.onConfigurationChanged(config);
+
+        /* Not need to be called if your application's minSdkVersion > = 14 */
+        RePlugin.App.onConfigurationChanged(config);
     }
 
 }
